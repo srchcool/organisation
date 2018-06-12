70c780f73482454b4bf8b1be33062fe9517f6895
Roumen Petrov <systerminator@gmail.com>
2018-06-12 14:40:26 +0100

    Updated dashed properties to camelCase and updated validation schema for naturally optional keys.

--- organisation.js
+++ organisation.js
@@ -12,36 +12,36 @@ function organisation(){
 
 body:
 {
-	  "type": "corp",
-	  "company-name": "Test Inc",
-	  "address":
+	  type: "corp",
+	  companyName: "Test Inc",
+	  address:
 	    {
-	      "company-address-line-one": "123 Test St",
-	      "company-address-line-two": "Suite 2A",
-	      "company-address-line-three": "Bldg 5",
-	      "city": "Test City",
-	      "postcode": "EH2 5AD",
-	      "state": "GA",
-	      "zipcode": "30033"
+	      companyAddressLineOne: "123 Test St",
+	      companyAddressLineTwo: "Suite 2A",
+	      companyAddressLineThree: "Bldg 5",
+	      city: "Test City",
+	      postcode: "EH2 5AD",
+	      state: "GA",
+	      zipcode: "30033"
 	    },
 	  "billing-address":
 	    {
-	      "company-address-line-one": "123 Test St Attn: Billing",
-	      "company-address-line-two": "Suite 2A",
-	      "company-address-line-three": "Bldg 5",
-	      "city": "Test Billing City",
-	      "postcode": "EH2 5AD",
-	      "state": "GA",
-	      "zipcode": "30033"
+	      companyAddressLineOne: "123 Test St Billing",
+	      companyAddressLineTwo: "Suite 2A Billing",
+	      companyAddressLineThree: "Bldg 5 Billing",
+	      city: "Test City Billing",
+	      postcode: "EH2 5AD",
+	      state: "GA",
+	      zipcode: "30033"
 	    },
-	   "company-telephone": "4043209989",
-	   "company-contact": "John Doe",
-	   "company-size": "10000",
-	   "vatnumber": "123456789",
-	   "company-number": "987654321",
-	   "employees":
+	   companyTelephone: "4043209989",
+	   companyContact: "John Doe",
+	   companySize: "10000",
+	   vatnumber: "123456789",
+	   companyNumber: "987654321",
+	   employees:
 	   {
-	    "customer-id": ["31a9923b-9ee1-4e9e-a3d4-8f800fabce54","31a9923b-9ee1-4e9e-a3d4-8f800fabce54"]
+	    customerId: ["31a9923b-9ee1-4e9e-a3d4-8f800fabce54","31a9923b-9ee1-4e9e-a3d4-8f800fabce54"]
 	   }
 }
 
@@ -66,21 +66,21 @@ body:
 
 	const addressSchema = ()=> {
 	  return( (0, objectSchema)().keys({
-	    	"company-address-line-one": (0, stringSchema)().required(),
-	    	"company-address-line-two": (0, stringSchema)(),
-	    	"company-address-line-three": (0, stringSchema)(),
-	    	"city": (0, stringSchema)().required(),
-	    	"postcode": (0, stringSchema)(),
-	    	"state": (0, stringSchema)(),
-	    	"zipcode": (0, stringSchema)()
-	    }).required() ); 
+	    	companyAddressLineOne: (0, stringSchema)().required(),
+	    	companyAddressLineTwo: (0, stringSchema)().optional(),
+	    	companyAddressLineThree: (0, stringSchema)().optional(),
+	    	city: (0, stringSchema)().required(),
+	    	postcode: (0, stringSchema)().optional(),
+	    	state: (0, stringSchema)().optional(),
+	    	zipcode: (0, stringSchema)().optional()
+	    }) ); 
 	};
 
 	const employeesSchema = ()=> {
 	  return( (0, objectSchema)().keys({
-	    	"customer-id": vandium.types.array().items(
+	    	customerId: vandium.types.array().items(
 	    		(0, uuidSchema)()
-	    	)
+	    	).optional()
 	    }) ); 
 	};	
 
@@ -88,24 +88,23 @@ body:
 		  body: {
 					id: (0, uuidSchema)(),
 					type: (0, stringSchema)().required(),
-					"company-name": (0, stringSchema)().required(),
-					"address": (0, addressSchema)().required(),
-					"billing-address": (0, addressSchema)().required(),
-					"company-telephone": (0, integerSchema)().required(),
-					"company-contact": (0, stringSchema)().required(),
-					"company-size": (0, integerSchema)().required(),
-					"vatnumber": (0, integerSchema)().required(),
-					"company-number": (0, integerSchema)().required(),
-					"employees": (0, employeesSchema)().required()
+					companyName: (0, stringSchema)().required(),
+					address: (0, addressSchema)().required(),
+					billingAddress: (0, addressSchema)().required(),
+					companyTelephone: (0, integerSchema)().required(),
+					companyContact: (0, stringSchema)().optional(),
+					companySize: (0, integerSchema)().optional(),
+					vatnumber: (0, stringSchema)().optional(),
+					companyNumber: (0, stringSchema)().optional(),
+					employees: (0, employeesSchema)().optional()
 
 		  }
 	};
 
 
-	const organisationRequiredKeys = ["type","company-name","address","billing-address","company-telephone",
-										"company-contact", "company-size", "vatnumber", "company-number",
-											"employees" ];
-	const organisationOptionalKeys = [];
+	const organisationRequiredKeys = ["type","companyName","address","billingAddress","companyTelephone"];
+
+	const organisationOptionalKeys = ["companyContact","companySize","vatnumber","companyNumber","employees"];
 
 	return(
 	{
